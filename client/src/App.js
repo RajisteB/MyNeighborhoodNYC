45868d4e75453a3963a4bbfef33f0d9652d3d2a7
Rajiste Bennin <rajistebennin@Rajistes-MBP.home>
2017-09-26 20:49:07 -0400

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchTerm: null,
+    }
+    
+  }
+  render() {
+    return (
+      <div className="App">
+       Testing...
+      </div>
+    );
+  }
+}
+
+export default App;
