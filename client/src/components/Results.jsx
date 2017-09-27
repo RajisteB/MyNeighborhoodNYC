936c990a3595df1a17442b4a4f1e268dc4092962
Rajiste Bennin <rajistebennin@Rajistes-MBP.home>
2017-09-27 10:02:20 -0400

    limit to open violations only

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 
 class Results extends Component {
     render() {
+
+        var house = document.getElementById('houseinput').value;
+        var street = document.getElementById('streetname').value.toUpperCase();
+        var zip = document.getElementById('zip').value;
+
         return(
             <div className="search_results">
-                Results component
+                <div>Results for: {house} {street} {this.props.data[0].boro}, NY {zip}</div>
                 {this.props.data.map(x => {
-                    return <h4>{x.novdescription}</h4>
+                    if (x.violationstatus === 'Open') {
+                    return <div key={x.violationid}>
+                    <h6>{x.violationstatus}</h6>
+                    <h6>{x.novdescription}</h6>
+                    </div>
+                    }
                 })}
             </div>
         )
